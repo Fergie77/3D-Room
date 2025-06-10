9617086b8257fbf99309c12eb0d78b52bad4e718
Fergie77 <fergie2@live.co.uk>
2025-06-10 11:21:04 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const camera = new THREE.PerspectiveCamera(
 )
 // Move camera back on mobile
 if (window.innerWidth < 700) {
-  camera.position.set(0, -0.3, -7)
+  camera.position.set(0, -0.3, -1)
 } else {
   camera.position.set(0, -0.3, -5)
 }
