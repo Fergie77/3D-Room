d5abe63fa6b40eef23ac9af23fe7ea8628d5ef0b
Fergie77 <fergie2@live.co.uk>
2025-06-10 14:48:50 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 controls.dampingFactor = 0.05
 controls.enablePan = true
-//controls.enableZoom = true
+controls.enableZoom = false
 controls.target.copy(camera.position).add(new THREE.Vector3(0, 0, -1)) // Look 1 unit in front of camera
 controls.update()
 controls.rotateSpeed = -0.5 // Invert rotation direction
