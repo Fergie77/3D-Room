55e921b83352ef68ce167585d78de340ccb2cc0b
Fergie77 <fergie2@live.co.uk>
2025-06-19 11:01:33 +0100

    cc

--- src/main.js
+++ src/main.js
@@ -63,8 +63,8 @@ if (!mainWrapper) {
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 controls.dampingFactor = 0.05
-controls.enablePan = true
-controls.enableZoom = true
+controls.enablePan = false
+controls.enableZoom = false
 controls.target.copy(camera.position).add(new THREE.Vector3(0, 0, -1))
 controls.update()
 controls.rotateSpeed = -0.5
